2da6ada98f44f2003b6d27ada23c8e5c110a193a
Mark Lallego <lallegomark@gmail.com>
2024-11-01 15:24:19 +0800

    New Files

--- Problem (Exercises)/Triangle Problem/triangle.c
+++ Problem (Exercises)/Triangle Problem/triangle.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
+
+/*
+Write a program that reads three floating values and check if it is possible to make a triangle with them. 
+Also calculate the perimeter of the triangle if the entered values are valid
+*/
 int main() {
 
+    //Declare the variables
     char name[80];
     float  x, y, z, perimeter;
 
+    //Get the name
     printf("Hello, please enter your name: ");
     scanf("%s", name);
     
+    //Get the three floating value
     printf("Hello, %s. Please enter the first number: ", name); 
     scanf("%f", &x);
     
@@ -16,7 +24,7 @@ int main() {
     printf("Enter the third number: ");
     scanf("%f", &z);
 
-    
+    //Check and Caluclate if the triangle is valid.
     if(x < (y+z) && y < (x+z) && z < (y+x)) 
         {  
         perimeter = x + y + z;
