8b94f51bfc6287ab21cc1f85e0fdc71a0d120047
Mark Lallego <lallegomark@gmail.com>
2024-11-06 23:12:25 +0800

    Update

--- Problem (Exercises)/Days Problem/days.c
+++ Problem (Exercises)/Days Problem/days.c
@@ -8,16 +8,16 @@ int main()
     printf("Hello, please enter your name: ");
     scanf("%s", name);
 
-    printf("Hello, %s, please enter number of day/s: ");
+    printf("Hello, %s, please enter number of day/s: ", name);
     scanf("%d", &days);
 
     day = days;
 
     years = days / 365;
-    days = days % 365;
+    days %= 365;
 
     weeks = days / 7;
-    day = days % 7;
+    days %= 7; 
 
     printf("%d days = %d year/s, %d week/s, and %d day/s", day, years, weeks, days);
 }
