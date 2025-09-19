e6af23150b0c54e2fb937f2d14cc942bd691bf2c
Aprien22 <lallegomark@gmail.com>
2025-09-19 15:21:11 +0800

    Commit All Changes

--- hello.py
+++
@@ -1,9 +0,0 @@
-def add(a: int, b: int) -> int:
-    return a + b
-
-x: int = int(input("Enter first number: "))
-y: int = int(input("Enter second number: "))
-
-result: int = add(x ,y)
-print(f"The sum of {x} and {y} is {result}")
-
